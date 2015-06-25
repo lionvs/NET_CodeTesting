3149107c5a672330dd553fc905c1dff9879533dc
Kyrylo M <kirmir@outlook.com>
2015-06-25 09:36:08 +0300

    Implemented an example of bad code that can't be tested.

---
+++ NetTesting/CashboxBad/Cashbox/DataAccess/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Cashbox.DataAccess.Models
+{
+    internal class Account
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public decimal Balance { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ NetTesting/CashboxGood/Cashbox/DataAccess/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Cashbox.DataAccess.Models
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public int Amount { get; set; }
+    }
+}
